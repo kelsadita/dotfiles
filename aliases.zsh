f3177ac36a6a583b4f02289530906cc273afe51f
Kalpesh Adhatrao <kalpesh.adhatrao@healthfidelity.com>
2017-08-01 10:06:45 +0530

    Added maven command to clean the rules.

--- aliases.zsh
+++ aliases.zsh
@@ -1,2 +1,5 @@
 # Frequent maven command aliases
 alias mct="mvn clean test -Dfindbugs.skip=true"
+
+# Coding rule engine formatting util
+alias clean-rule="mvn clean compile && mvn exec:exec -Dformat"
